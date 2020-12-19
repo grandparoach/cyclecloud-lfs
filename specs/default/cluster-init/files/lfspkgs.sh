966432e0780a7c15ffe37e8f1d75ce3dd1a3b0e4
Jerry Morey <24438151+themorey@users.noreply.github.com>
2020-12-19 17:32:31 -0500

    Update lfspkgs.sh

--- specs/default/cluster-init/files/lfspkgs.sh
+++ specs/default/cluster-init/files/lfspkgs.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-# Install the Lustre-patched kernel packages
-yum --nogpgcheck --disablerepo=base,extras,updates \
---enablerepo=lustreserver install \
-kernel \
-kernel-devel \
-kernel-headers \
-kernel-tools \
-kernel-tools-libs \
-kernel-tools-libs-devel
-
 yum -y install lustre kmod-lustre-osd-ldiskfs lustre-osd-ldiskfs-mount lustre-resource-agents e2fsprogs lustre-tests
 
 sed -i 's/ResourceDisk\.Format=y/ResourceDisk.Format=n/g' /etc/waagent.conf
